510ca2bad17f0ea0258c260345534b28b14ac2bb
Emman Patricio <emanpatricio@gmail.com>
2020-12-14 22:00:43 +0800

    added no restart

--- config.h
+++ config.h
@@ -2,12 +2,9 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizpadbar        = 7;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 7;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
